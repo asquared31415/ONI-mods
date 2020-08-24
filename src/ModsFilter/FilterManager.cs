3c3f6a25ea0a219e7a6e168322188c91bbd04338
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2020-08-24 19:39:55 -0400

    Add Mod Filter (new repo yay!)

---
+++ src/ModsFilter/FilterManager.cs
@@ -0,0 +1,43 @@
+﻿using Harmony;
+using TMPro;
+
+namespace ModsFilter
+{
+	public class FilterManager
+	{
+		private readonly TMP_InputField _search;
+		private readonly KButton _clearSearchButton;
+
+		public FilterManager(TMP_InputField search, KButton button)
+		{
+			_search = search;
+			_clearSearchButton = button;
+		}
+
+		public string Text => _search.text;
+
+		public void ConfigureButtons(ModsScreen modsScreen)
+		{
+			_search.text = "";
+			_search.onValueChanged.AddListener(
+				_ =>
+				{
+					// This shouldn't ever be null, but good idea to check
+					if(modsScreen != null)
+					{
+						Traverse.Create(modsScreen)
+						        .Method("RebuildDisplay", typeof(object))
+						        .GetValue();
+					}
+				}
+			);
+
+			_clearSearchButton.onClick += () => _search.text = "";
+			var tt = _clearSearchButton.GetComponent<ToolTip>();
+			if(tt != null)
+			{
+				tt.toolTip = "Clear Search";
+			}
+		}
+	}
+}
