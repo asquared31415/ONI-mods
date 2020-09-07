ddd9bb47e2f276ad50a1bde1d42c043ea8ccbf27
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2020-09-06 22:22:20 -0400

    ModsFilter - fix crash

--- src/ModsFilter/FilterManager.cs
+++ src/ModsFilter/FilterManager.cs
@@ -1,4 +1,5 @@
-﻿using Harmony;
+﻿using System;
+using Harmony;
 using TMPro;
 
 namespace ModsFilter
@@ -8,33 +9,36 @@ namespace ModsFilter
 		private readonly TMP_InputField _search;
 		private readonly KButton _clearSearchButton;
 
-		public FilterManager(TMP_InputField search, KButton button)
+		public FilterManager( TMP_InputField search, KButton button )
 		{
+			if( search == null || button == null )
+			{
+				throw new ArgumentException( "[ModsFilter] null search field or clear button!" );
+			}
+
 			_search = search;
 			_clearSearchButton = button;
 		}
 
 		public string Text => _search.text;
 
-		public void ConfigureButtons(ModsScreen modsScreen)
+		public void ConfigureButtons( ModsScreen modsScreen )
 		{
 			_search.text = "";
 			_search.onValueChanged.AddListener(
 				_ =>
 				{
 					// This shouldn't ever be null, but good idea to check
-					if(modsScreen != null)
+					if( modsScreen != null )
 					{
-						Traverse.Create(modsScreen)
-						        .Method("RebuildDisplay", typeof(object))
-						        .GetValue();
+						Traverse.Create( modsScreen ).Method( "RebuildDisplay", typeof(object) ).GetValue();
 					}
 				}
 			);
 
 			_clearSearchButton.onClick += () => _search.text = "";
 			var tt = _clearSearchButton.GetComponent<ToolTip>();
-			if(tt != null)
+			if( tt != null )
 			{
 				tt.toolTip = "Clear Search";
 			}
--- src/ModsFilter/ModsFilterPatches.cs
+++ src/ModsFilter/ModsFilterPatches.cs
@@ -12,68 +12,67 @@ namespace ModsFilter
 		private static ModsScreen _modsScreen;
 		private static GameObject _prefab;
 
-		[HarmonyPatch(typeof(MainMenu), "OnPrefabInit")]
+		[HarmonyPatch( typeof(MainMenu), "OnPrefabInit" )]
 		public static class MainMenu_OnPrefabInit_Patch
 		{
 			public static void Postfix()
 			{
 				var prefabGo = ScreenPrefabs.Instance.RetiredColonyInfoScreen.gameObject;
-				prefabGo.SetActive(false);
-				var clone = Util.KInstantiateUI(prefabGo);
-				if(clone != null)
+				prefabGo.SetActive( false );
+				var clone = Util.KInstantiateUI( prefabGo );
+				if( clone != null )
 				{
-					var go = clone.transform.Find("Content/ColonyData/Colonies and Achievements/Colonies/Search");
-					if(go != null)
+					var go = clone.transform.Find( "Content/ColonyData/Colonies and Achievements/Colonies/Search" );
+					if( go != null )
 					{
-						_prefab = Util.KInstantiateUI(go.gameObject);
-						Object.Destroy(clone);
-						prefabGo.SetActive(true);
+						_prefab = Util.KInstantiateUI( go.gameObject );
+						Object.Destroy( clone );
+						prefabGo.SetActive( true );
 
 						return;
 					}
 				}
 
 				// ERROR!
-				Debug.Log("[ModFilter] Error creating search prefab!  The mod will not function!");
+				Debug.Log( "[ModFilter] Error creating search prefab!  The mod will not function!" );
 			}
 		}
 
-		[HarmonyPatch(typeof(ModsScreen), "OnActivate")]
+		[HarmonyPatch( typeof(ModsScreen), "OnActivate" )]
 		public static class ModsScreen_OnActivate_Patch
 		{
-			public static void Prefix(ModsScreen __instance)
+			public static void Prefix( ModsScreen __instance )
 			{
-				var prefab = _prefab;
-				if(prefab == null)
+				if( _prefab == null )
 				{
 					return;
 				}
 
 				_modsScreen = __instance;
-				var local = Util.KInstantiateUI(prefab);
-				var panel = __instance.transform.Find("Panel");
-				if(panel != null)
+				var local = Util.KInstantiateUI( _prefab );
+				var panel = __instance.transform.Find( "Panel" );
+				if( panel != null )
 				{
 					var trans = local.transform;
-					trans.SetParent(panel, false);
-					trans.SetSiblingIndex(1);
-					local.SetActive(true);
+					trans.SetParent( panel, false );
+					trans.SetSiblingIndex( 1 );
+					local.SetActive( true );
 
 					_filterManager = new FilterManager(
-						trans.Find("LocTextInputField").GetComponent<TMP_InputField>(),
-						trans.Find("ClearButton").GetComponent<KButton>()
+						trans.Find( "LocTextInputField" ).GetComponent<TMP_InputField>(),
+						trans.Find( "ClearButton" ).GetComponent<KButton>()
 					);
 
-					_filterManager.ConfigureButtons(_modsScreen);
+					_filterManager.ConfigureButtons( _modsScreen );
 				}
 				else
 				{
-					Debug.Log("[ModFilter] Error adding search bar to mods screen!");
+					Debug.Log( "[ModFilter] Error adding search bar to mods screen!" );
 				}
 			}
 		}
 
-		[HarmonyPatch(typeof(ModsScreen), "OnDeactivate")]
+		[HarmonyPatch( typeof(ModsScreen), "OnDeactivate" )]
 		public static class ModsScreen_OnDeactivate_Patch
 		{
 			public static void Prefix()
@@ -84,15 +83,15 @@ namespace ModsFilter
 		}
 
 		// NOTE: This gets called within OnActivate as well
-		[HarmonyPatch(typeof(ModsScreen), "ShouldDisplayMod")]
+		[HarmonyPatch( typeof(ModsScreen), "ShouldDisplayMod" )]
 		public static class ModsScreen_ShouldDisplayMod_Patch
 		{
-			public static void Postfix(KMod.Mod mod, ref bool __result)
+			public static void Postfix( KMod.Mod mod, ref bool __result )
 			{
-				if(__result && _filterManager != null)
+				if( __result && _filterManager != null )
 				{
 					var text = _filterManager.Text;
-					if(!string.IsNullOrEmpty(text))
+					if( !string.IsNullOrEmpty( text ) )
 					{
 						__result = CultureInfo.InvariantCulture.CompareInfo.IndexOf(
 							           mod.label.title,
@@ -105,31 +104,31 @@ namespace ModsFilter
 			}
 		}
 
-		[HarmonyPatch(typeof(ModsScreen), "UpdateToggleAllButton")]
+		[HarmonyPatch( typeof(ModsScreen), "UpdateToggleAllButton" )]
 		public static class ModsScreen_UpdateToggleAllButton_Patch
 		{
 			// If we are not searching, let the button update
-			public static bool Prefix() => string.IsNullOrEmpty(_filterManager.Text);
+			public static bool Prefix() => _filterManager == null || string.IsNullOrEmpty( _filterManager.Text );
 		}
 
-		[HarmonyPatch(typeof(ModsScreen), "BuildDisplay")]
+		[HarmonyPatch( typeof(ModsScreen), "BuildDisplay" )]
 		public static class ModsScreen_BuildDisplay_Patch
 		{
 			// Update the toggle button's state every time we build the display
-			public static void Prefix(KButton ___toggleAllButton)
+			public static void Prefix( KButton ___toggleAllButton )
 			{
-				var isEmpty = string.IsNullOrEmpty(_filterManager.Text);
+				var isEmpty = _filterManager == null || string.IsNullOrEmpty( _filterManager.Text );
 				___toggleAllButton.isInteractable = isEmpty;
-				if(!isEmpty)
+				if( !isEmpty )
 				{
 					___toggleAllButton.gameObject.AddOrGet<ToolTip>().toolTip = "Disabled while filter is active";
 				}
 				else
 				{
 					var tt = ___toggleAllButton.gameObject.GetComponent<ToolTip>();
-					if(tt != null)
+					if( tt != null )
 					{
-						Object.Destroy(tt);
+						Object.Destroy( tt );
 					}
 				}
 			}
