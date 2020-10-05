9232b391371d5270ab44746274c089ec02b996fe
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2020-10-05 08:59:40 -0400

    Fix spacing, optional folder for sprites

--- src/SquareLib/ModAssets.cs
+++ src/SquareLib/ModAssets.cs
@@ -5,60 +5,65 @@ using UnityEngine;
 
 namespace SquareLib
 {
-	public static class ModAssets
-	{
-		private static readonly TextureAtlas TileAtlas = global::Assets.GetTextureAtlas("tiles_solid");
+    public static class ModAssets
+    {
+        private static readonly TextureAtlas TileAtlas = global::Assets.GetTextureAtlas("tiles_solid");
 
-		public static TextureAtlas GetCustomTileAtlas(string name)
-		{
-			var path = Path.Combine(Path.GetDirectoryName(Assembly.GetCallingAssembly().Location), name + ".png");
-			TextureAtlas textureAtlas = null;
-			try
-			{
-				byte[] data = File.ReadAllBytes(path);
-				var tex = new Texture2D(2, 2);
-				tex.LoadImage(data);
-				textureAtlas = ScriptableObject.CreateInstance<TextureAtlas>();
-				textureAtlas.texture = tex;
-				textureAtlas.vertexScale = TileAtlas.vertexScale;
-				textureAtlas.items = TileAtlas.items;
-			}
-			catch
-			{
-				Debug.LogError($"[Item Permeable Tiles]: Could not load atlas image at path {path}");
-			}
+        public static TextureAtlas GetCustomTileAtlas(string name)
+        {
+            var path = Path.Combine(Path.GetDirectoryName(Assembly.GetCallingAssembly().Location), name + ".png");
+            TextureAtlas textureAtlas = null;
+            try
+            {
+                byte[] data = File.ReadAllBytes(path);
+                var tex = new Texture2D(2, 2);
+                tex.LoadImage(data);
+                textureAtlas = ScriptableObject.CreateInstance<TextureAtlas>();
+                textureAtlas.texture = tex;
+                textureAtlas.vertexScale = TileAtlas.vertexScale;
+                textureAtlas.items = TileAtlas.items;
+            }
+            catch
+            {
+                Debug.LogError($"[Item Permeable Tiles]: Could not load atlas image at path {path}");
+            }
 
-			return textureAtlas;
-		}
+            return textureAtlas;
+        }
 
-		public static void AddSpriteFromFile(string name)
-		{
-			var texture = LoadTexture(name);
+        public static void AddSpriteFromFile(string name)
+        {
+            var texture = LoadTexture(name);
 
-			var sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(texture.width / 2f, texture.height / 2f));
-			Assets.Sprites.Add(name, sprite);
-		}
+            var sprite = Sprite.Create(
+                texture,
+                new Rect(0, 0, texture.width, texture.height),
+                new Vector2(texture.width / 2f, texture.height / 2f)
+            );
 
-		public static Texture2D LoadTexture(string name)
-		{
-			Texture2D texture = null;
-			var directory = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "assets");
+            Assets.Sprites.Add(name, sprite);
+        }
 
-			var texFile = Path.Combine(directory, $"{name}.png");
+        public static Texture2D LoadTexture(string name, string folder = null)
+        {
+            Texture2D texture = null;
+            var directory = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), folder ?? "assets");
 
-			try
-			{
-				byte[] data = File.ReadAllBytes(texFile);
-				texture = new Texture2D(1, 1);
-				texture.LoadImage(data);
-			}
-			catch(Exception e)
-			{
-				Debug.LogError($"Could not load texture at assets/{texFile}.png");
-				Debug.LogException(e);
-			}
+            var texFile = Path.Combine(directory, $"{name}.png");
 
-			return texture;
-		}
-	}
+            try
+            {
+                byte[] data = File.ReadAllBytes(texFile);
+                texture = new Texture2D(1, 1);
+                texture.LoadImage(data);
+            }
+            catch(Exception e)
+            {
+                Debug.LogError($"Could not load texture at {folder ?? "assets"}/{texFile}.png");
+                Debug.LogException(e);
+            }
+
+            return texture;
+        }
+    }
 }
