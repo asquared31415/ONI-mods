60408f66827e18dce6dda19c1b992a0e39bdac3a
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2020-10-20 00:16:51 -0400

    Meep

---
+++ src/Meep/Meep.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using Database;
+using Harmony;
+
+namespace Meep
+{
+    [HarmonyPatch(typeof(Personalities), MethodType.Constructor, new Type[0])]
+    public class Meep
+    {
+        public static void OnLoad()
+        {
+            Debug.Log(
+                $"[Meep] Meep version {FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion}"
+            );
+        }
+
+        public static void Postfix(Personalities __instance)
+        {
+            var meeps = __instance.resources.Count;
+            var meep = __instance.resources.Find(p => p.Id == "Meep");
+            for(var i = 0; i < meeps; ++i)
+                __instance.resources[i] = meep;
+        }
+    }
+}
---
+++ src/Meep/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Meep")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("Meep")]
+[assembly: AssemblyCopyright("Copyright © asquared31415 2020")]
+
+[assembly: AssemblyVersion("0.0.0.1")]
