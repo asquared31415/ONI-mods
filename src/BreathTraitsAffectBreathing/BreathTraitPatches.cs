3e6d3285019e516b4b1cd88c5363bd1b8cce5a72
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2021-07-23 16:24:20 -0400

    Breath Traits Affect Suffocation update

--- src/BreathTraitsAffectBreathing/BreathTraitPatches.cs
+++ src/BreathTraitsAffectBreathing/BreathTraitPatches.cs
@@ -1,37 +1,27 @@
+using HarmonyLib;
 using Klei.AI;
-using STRINGS;
 
-namespace BreathTraitsAffectBreathing
+namespace BreathTraitsAffectSuffocation
 {
 	[HarmonyPatch(typeof(SuffocationMonitor.Instance), MethodType.Constructor, typeof(OxygenBreather))]
-	internal class BreathTraitPatches
+	public static class BreathTraitPatches
 	{
 		public static void Postfix(ref SuffocationMonitor.Instance __instance, ref OxygenBreather oxygen_breather)
 		{
 			var minionIdentity = oxygen_breather.gameObject.GetComponent<MinionIdentity>();
 			if (minionIdentity.GetComponent<Traits>().HasTrait("DiversLung"))
 			{
-				__instance.holdingbreath = new AttributeModifier(
-					Db.Get().Amounts.Breath.deltaAttribute.Id,
-					-0.6818182f,
-					DUPLICANTS.MODIFIERS.HOLDINGBREATH.NAME
-				);
+				__instance.holdingbreath.SetValue(__instance.holdingbreath.Value * 3f / 4f);
 			}
-			else if (minionIdentity.GetComponent<Traits>().HasTrait("MouthBreather"))
+
+			if (minionIdentity.GetComponent<Traits>().HasTrait("MouthBreather"))
 			{
-				__instance.holdingbreath = new AttributeModifier(
-					Db.Get().Amounts.Breath.deltaAttribute.Id,
-					-1.8181818f,
-					DUPLICANTS.MODIFIERS.HOLDINGBREATH.NAME
-				);
+				__instance.holdingbreath.SetValue(__instance.holdingbreath.Value * 2);
 			}
-			else if (minionIdentity.GetComponent<Traits>().HasTrait("DeeperDiversLungs"))
+
+			if (minionIdentity.GetComponent<Traits>().HasTrait("DeeperDiversLungs"))
 			{
-				__instance.holdingbreath = new AttributeModifier(
-					Db.Get().Amounts.Breath.deltaAttribute.Id,
-					-0.454545f,
-					DUPLICANTS.MODIFIERS.HOLDINGBREATH.NAME
-				);
+				__instance.holdingbreath.SetValue(__instance.holdingbreath.Value / 2);
 			}
 		}
 	}
