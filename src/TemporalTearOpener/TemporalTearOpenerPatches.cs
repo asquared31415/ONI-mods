4e98ca52beebfd1ece67fe183f74a1a5f1121975
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2022-02-11 03:51:50 -0500

    Temporal Tear Opener mod

---
+++ src/TemporalTearOpener/TemporalTearOpenerPatches.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using KMod;
+
+namespace TemporalTearOpener
+{
+	public class TemporalTearOpenerInfo : UserMod2
+	{
+	}
+
+	[HarmonyPatch(typeof(TemporalTear), "OnSpawn")]
+	public static class TemporalTear_OnSpawn_Patch
+	{
+		public static void Prefix(ref bool ___m_open)
+		{
+			___m_open = true;
+		}
+	}
+}
