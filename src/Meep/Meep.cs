4f9415c3f62496f4bbc54d711b82ef370c68decd
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2022-09-15 23:05:03 -0400

    add archived versions to git, update meep to public beta

--- src/Meep/Meep.cs
+++ src/Meep/Meep.cs
@@ -1,25 +1,36 @@
 using System;
 using Database;
 using HarmonyLib;
+using JetBrains.Annotations;
 using KMod;
 
-namespace Meep
-{
-	public class MeepInfo : UserMod2
-	{
-	}
+namespace Meep;
 
-	[HarmonyPatch(typeof(Personalities), MethodType.Constructor, new Type[0])]
-	public class Meep
+[UsedImplicitly]
+public class MeepInfo : UserMod2
+{
+}
+
+[HarmonyPatch(typeof(Personalities), MethodType.Constructor, new Type[0])]
+public static class Meep
+{
+	private const string MeepId = "Meep";
+
+	[UsedImplicitly]
+	public static void Postfix(Personalities __instance)
 	{
-		public static void Postfix(Personalities __instance)
+		var meep = __instance.resources.Find(p => p.Id == MeepId);
+		if (meep == null)
 		{
-			var meeps = __instance.resources.Count;
-			var meep = __instance.resources.Find(p => p.Id == "Meep");
-			for (var i = 0; i < meeps; ++i)
-			{
-				__instance.resources[i] = meep;
-			}
+			Debug.LogWarning("[Meep] Unable to find Meep, cannot make him the only dupe!");
+			return;
 		}
+
+		// make Meep a starting minion to make sure that a valid starting minion exists
+		meep.startingMinion = true;
+
+		// remove all other dupes
+		// as of recent updates, the game does not hard code expected lengths, which makes this much simpler
+		__instance.resources.RemoveAll(p => p.Id != MeepId);
 	}
 }
