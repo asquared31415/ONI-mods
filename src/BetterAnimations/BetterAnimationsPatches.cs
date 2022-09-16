6343ebaf2c30be869772b6c6c184d526fc4483bb
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2022-09-16 18:02:28 -0400

    better animations archived versions

--- src/BetterAnimations/BetterAnimationsPatches.cs
+++ src/BetterAnimations/BetterAnimationsPatches.cs
@@ -3,82 +3,85 @@ using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using HarmonyLib;
+using JetBrains.Annotations;
 using Klei.AI;
 using KMod;
 using UnityEngine;
 
-namespace BetterAnimations
+namespace BetterAnimations;
+
+[UsedImplicitly]
+public class BetterAnimationsInfo : UserMod2
 {
-	public class BetterAnimationsInfo : UserMod2
-	{
-	}
+}
 
-	public static class BetterAnimationsPatches
+public static class BetterAnimationsPatches
+{
+	[HarmonyPatch]
+	public static class MultitoolController_Patch
 	{
-		[HarmonyPatch]
-		public static class MultitoolController_Patch
+		private static readonly MethodInfo PlayMethodInfo = AccessTools.Method(
+			typeof(KAnimControllerBase),
+			"Play",
+			new[]
+			{
+				typeof(HashedString),
+				typeof(KAnim.PlayMode),
+				typeof(float), typeof(float),
+			}
+		);
+
+		private static readonly MethodInfo SpeedHelper = AccessTools.Method(
+			typeof(MultitoolController_Patch),
+			nameof(GetScaleHelper)
+		);
+
+		[UsedImplicitly]
+		public static IEnumerable<MethodInfo> TargetMethods()
 		{
-			private static readonly MethodInfo PlayMethodInfo = AccessTools.Method(
-				typeof(KAnimControllerBase),
-				"Play",
-				new[]
-				{
-					typeof(HashedString),
-					typeof(KAnim.PlayMode),
-					typeof(float), typeof(float),
-				}
-			);
+			yield return AccessTools.Method(typeof(MultitoolController.Instance), "PlayPre");
+			yield return AccessTools.Method(typeof(MultitoolController.Instance), "PlayPost");
+		}
 
-			private static readonly MethodInfo SpeedHelper = AccessTools.Method(
-				typeof(MultitoolController_Patch),
-				nameof(GetScaleHelper)
-			);
-
-			public static IEnumerable<MethodInfo> TargetMethods()
+		[UsedImplicitly]
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> orig)
+		{
+			var codes = orig.ToList();
+			var idx = codes.FindIndex(ci => ci.operand is MethodInfo m && (m == PlayMethodInfo));
+			if (idx != -1)
 			{
-				yield return AccessTools.Method(typeof(MultitoolController.Instance), "PlayPre");
-				yield return AccessTools.Method(typeof(MultitoolController.Instance), "PlayPost");
+				--idx;
+				// Insert after the load of the speed mult
+				codes.Insert(idx++, new CodeInstruction(OpCodes.Ldarg_0));
+				codes.Insert(idx, new CodeInstruction(OpCodes.Call, SpeedHelper));
+			}
+			else
+			{
+				Debug.LogError("Could not patch multitool animation speed");
 			}
 
-			public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> orig)
-			{
-				var codes = orig.ToList();
-				var idx = codes.FindIndex(ci => ci.operand is MethodInfo m && m == PlayMethodInfo);
-				if (idx != -1)
-				{
-					--idx;
-					// Insert after the load of the speed mult
-					codes.Insert(idx++, new CodeInstruction(OpCodes.Ldarg_0));
-					codes.Insert(idx, new CodeInstruction(OpCodes.Call, SpeedHelper));
-				}
-				else
-				{
-					Debug.LogError("Could not patch multitool animation speed");
-				}
+			return codes;
+		}
 
-				return codes;
+		// At level 20, should be 3.0
+		// Clamp to no less than 0.75
+		private static float GetScaleHelper(float scale, MultitoolController.Instance smi)
+		{
+			// manual implementation for tryget
+			var go = smi.sm.worker.Get(smi);
+			if (go != null)
+			{
+				var levels = go.GetComponent<AttributeLevels>();
+
+				if (levels != null)
+				{
+					var athletics = levels.GetAttributeLevel("Athletics");
+					return scale * Mathf.Clamp(athletics.level / 6.6667f, 0.75f, float.PositiveInfinity);
+				}
 			}
 
-			// At level 20, should be 3.0
-			// Clamp to no less than 0.75
-			private static float GetScaleHelper(float scale, MultitoolController.Instance smi)
-			{
-				// manual implementation for tryget
-				var go = smi.sm.worker.Get(smi);
-				if (go != null)
-				{
-					var levels = go.GetComponent<AttributeLevels>();
-
-					if (levels != null)
-					{
-						var athletics = levels.GetAttributeLevel("Athletics");
-						return scale * Mathf.Clamp(athletics.level / 6.6667f, 0.75f, float.PositiveInfinity);
-					}
-				}
-
-				// failsafe to 1.0x if the worker doesn't have any attributes or Athletics (possible on rover)
-				return 1f;
-			}
+			// failsafe to 1.0x if the worker doesn't have any attributes or Athletics (possible on rover)
+			return 1f;
 		}
 	}
 }
