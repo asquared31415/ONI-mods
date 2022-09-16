511ae1a73706a5478d83765a33d15d681384f9e3
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2022-09-16 14:34:45 -0400

    move in no mop limit archive

--- src/NoMopLimit/NoMopLimits.cs
+++ src/NoMopLimit/NoMopLimits.cs
@@ -3,37 +3,39 @@ using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using HarmonyLib;
+using JetBrains.Annotations;
 using KMod;
 
-namespace NoMopLimit
+namespace NoMopLimit;
+
+[UsedImplicitly]
+public class NoMopLimitInfo : UserMod2
 {
-	public class NoMopLimitInfo : UserMod2
+	public override void OnLoad(Harmony harmony)
 	{
-		public override void OnLoad(Harmony harmony)
-		{
-			MopTool.maxMopAmt = float.PositiveInfinity;
-			base.OnLoad(harmony);
-		}
+		MopTool.maxMopAmt = float.PositiveInfinity;
+		base.OnLoad(harmony);
 	}
+}
 
-	[HarmonyPatch(typeof(MopTool), "OnDragTool")]
-	public static class MopTool_OnDragTool_Patch
+[HarmonyPatch(typeof(MopTool), "OnDragTool")]
+public static class MopTool_OnDragTool_Patch
+{
+	public static readonly MethodInfo CellBelowInfo = AccessTools.Method(typeof(Grid), "CellBelow");
+
+	[UsedImplicitly]
+	public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> orig)
 	{
-		public static readonly MethodInfo CellBelowInfo = AccessTools.Method(typeof(Grid), "CellBelow");
-
-		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> orig)
+		var codes = orig.ToList();
+		var idx = codes.FindIndex(ci => ci.operand is MethodInfo i && (i == CellBelowInfo));
+		if (idx != -1)
 		{
-			var codes = orig.ToList();
-			var idx = codes.FindIndex(ci => ci.operand is MethodInfo i && i == CellBelowInfo);
-			if (idx != -1)
-			{
-				var i = idx - 2;
-				// Remove all before store to flag
-				codes.RemoveRange(i, 4);
-				codes.Insert(i, new CodeInstruction(OpCodes.Ldc_I4_1));
-			}
-
-			return codes;
+			var i = idx - 2;
+			// Remove all before store to flag
+			codes.RemoveRange(i, 4);
+			codes.Insert(i, new CodeInstruction(OpCodes.Ldc_I4_1));
 		}
+
+		return codes;
 	}
 }
