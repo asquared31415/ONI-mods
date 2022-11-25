1661e23eebf8e4ce814d8d83268bf1e983fb6da2
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2022-11-24 22:05:06 -0500

    enable chef skill

---
+++ src/EnableChefHat/EnableChefHat.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using JetBrains.Annotations;
+using KMod;
+
+namespace EnableChefHat;
+
+public class EnableChefHat : UserMod2
+{
+}
+
+[HarmonyPatch(typeof(MinionResume), nameof(MinionResume.IsAbleToLearnSkill))]
+public class MinionResume_IsAbleToLearnSkill_Patch
+{
+	[UsedImplicitly]
+	public static void Postfix(ref bool __result, string skillId)
+	{
+		if (skillId == Db.Get().Skills.Cooking1.Id)
+		{
+			__result = true;
+		}
+	}
+}
